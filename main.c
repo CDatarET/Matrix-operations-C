9ed77eba271e316af48184babb80493ea9458584
Chaitanya Datar <datar.chaitanya@gmail.com>
2025-03-11 20:45:02 +0530

    Update main.c

--- main.c
+++ main.c
@@ -11,8 +11,8 @@ int main() {
 
     printf("Enter for matrix 1: ");
     int a[rows][cols];
-    for(int i = 0; i < rows; i++){
-        for(int j = 0; j < cols; j++){
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
             printf("Enter element at position [%d][%d]:\n", i, j);
             scanf("%d", &a[i][j]);
         }
@@ -20,38 +20,61 @@ int main() {
 
     printf("Now enter for matrix 2: ");
     int b[rows][cols];
-    for(int i = 0; i < rows; i++){
-        for(int j = 0; j < cols; j++){
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
             printf("Enter element at position [%d][%d]:\n", i, j);
             scanf("%d", &b[i][j]);
         }
     }
 
     printf("The addition of Matrices is:\n");
-    for(int i = 0; i < rows; i++){
-        for(int j = 0; j < cols; j++){
-            printf("%d ", a[i][j] + b[i][j]);
+    int sum[rows][cols];
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            sum[i][j] = a[i][j] + b[i][j];
+            printf("%d ", sum[i][j]);
         }
         printf("\n");
     }
 
-    printf("finding saddle point of matrix: ");
+    int saddle = 0;
+    int saddleFound = 0;
+    int col;
+    int row;
     for(int i = 0; i < rows; i++){
-        int saddleNum = a[i][j];
-        for(int j = 0; j < cols; j++){
-            if(j < cols - 1){
-                if(a[i][j+1] > saddleNum){
-                    saddleNum = a[i][j+1];
-                }
+        saddle = a[i][0];
+        row = i;
+        col = 0;
+        for(int j = 0; j < cols - 1; j++){
+            if(saddle > a[i][j + 1]){
+                saddle = a[i][j+1];
+                col = j+1;
+            }
+            else{
+                continue;
             }
         }
+        int checkBreak = 0;
         for(int k = 0; k < rows; k++){
-            if(k < rows - 1){
-                
+            if(saddle < a[k][col]){
+                checkBreak = 1;
+                break;
             }
-            
+        }
+        if(!checkBreak){
+            saddleFound = 1;
+        }
+        if(saddleFound){
+            break;
         }
     }
+    
+    if(!saddleFound){
+            printf("no saddle");
+        }
+        else{
+            printf("saddle is %d found at (%d,%d)", saddle, row,col);
+        }
 
     return 0;
 }
